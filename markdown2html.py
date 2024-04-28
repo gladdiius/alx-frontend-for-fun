9bcefd7e945b546522ceed44ae174b3fd18cce9b
elias <eliaseshetu28@gmail.com>
2024-04-28 20:46:21 +0300

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -17,12 +17,12 @@ def main():
     """
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py <Markdown file> <Output file>", file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
     markdown_file = sys.argv[1]
 
     if not os.path.exists(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
-    sys.exit(0)
+    exit(0)
