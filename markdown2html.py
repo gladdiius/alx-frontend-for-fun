776b74cabbf6e34aa61b43f1801fb4f42b9bd3e9
elias <eliaseshetu28@gmail.com>
2024-04-28 15:20:18 +0300

    update

--- markdown2html.py
+++ markdown2html.py
@@ -3,16 +3,28 @@
 import sys
 import os
 
+def markdownToHtml():
+    """Converts Markdown file to HTML.
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    sys.exit(1)
+    Args:
+        markdown_file (str): The path to the Markdown file.
+        output_file (str): The path to the output HTML file.
 
-markdown_file = sys.argv[1]
-output_file = sys.argv[2]
+    Returns:
+        int: Exit code. 0 if successful, 1 if Markdown file is missing.
+    """
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
 
-if not os.path.exists(markdown_file):
-    sys.stderr.write(f"Missing {markdown_file}\n")
-    sys.exit(1)
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-sys.exit(0)
+    if not os.path.exists(markdown_file):
+        sys.stderr.write(f"Missing {markdown_file}\n")
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    markdownToHtml()
