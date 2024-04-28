4c4299976f9234baca410280af866697b35af99b
elias <eliaseshetu28@gmail.com>
2024-04-28 13:35:02 +0300

    markdown2html

--- markdown2html.py
+++ markdown2html.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 import sys
-import os
 
 
 def convert_md_to_html(input_file, output_file):
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        sys.exit(os.EX_USAGE)
+        sys.exit(1)
 
     if not input_file.endswith(".md"):
         print("Input file must have a .md extension", file=sys.stderr)
-        sys.exit(os.EX_USAGE)
+        sys.exit(1)
 
-    sys.exit(os.EX_OK)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
