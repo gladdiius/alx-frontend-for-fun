50a5897efc87ddf9ba531737228db73ed7b0c135
elias <eliaseshetu28@gmail.com>
2024-04-28 14:06:15 +0300

    updated

--- markdown2html.py
+++ markdown2html.py
@@ -18,15 +18,11 @@ def convert_md_to_html(input_file, output_file):
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
-
-    try:
-        if not os.path.exists(input_file):
-            raise FileNotFoundError(f"Missing {input_file}")
-    except FileNotFoundError as e:
-        print(e, file=sys.stderr)
+    if not os.path.exists(input_file):
+        print("Missing {input_file}", file=sys.stderr)
         sys.exit(1)
+    sys.exit(0)
 
 if __name__ == "__main__":
     input_file, output_file = sys.argv[1], sys.argv[2]
     convert_md_to_html(input_file, output_file)
-    sys.exit(0)
