66edbaa590183c866652d09a3d41297585dc9e59
elias <eliaseshetu28@gmail.com>
2024-04-28 19:11:38 +0300

    markdown2html

--- markdown2html.py
+++ markdown2html.py
@@ -16,15 +16,15 @@ def markdownToHtml():
     """
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        sys.exit(1)
+        exit(1)
 
     markdown_file = sys.argv[1]
 
     if not os.path.exists(markdown_file):
         sys.stderr.write(f"Missing {markdown_file}\n")
-        sys.exit(1)
+        exit(1)
 
-    sys.exit(0)
+    exit(0)
 
 if __name__ == "__main__":
     markdownToHtml()
