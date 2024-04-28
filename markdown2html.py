0456ccfcc787f9757243e7c734dfe6b421301062
elias <eliaseshetu28@gmail.com>
2024-04-28 21:06:20 +0300

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -19,16 +19,16 @@ def convert_markdown_to_html(markdown_text, output_file):
             f.write(f'<html><body>{markdown_text}</body></html>')
     except FileNotFoundError:
         sys.stderr.write(f"Missing {markdown_text}\n")
-        sys.exit(1)
+        exit(1)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        sys.exit(1)
+        exit(1)
 
     markdown_text = sys.argv[1]
     output_file = sys.argv[2]
 
     convert_markdown_to_html(markdown_text, output_file)
-    sys.exit(0)
+    exit(0)
