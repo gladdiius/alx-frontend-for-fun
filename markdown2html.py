2fcc5ef93e4035d8e350e5a275f9182def13f7bd
elias <eliaseshetu28@gmail.com>
2024-04-28 19:37:16 +0300

    commit

--- markdown2html.py
+++ markdown2html.py
@@ -1,30 +1,24 @@
-#!/usr/bin/python3
-"""markdown converter to html"""
+#!/usr/bin/env python3
+
 import sys
-import os
+import os.path
 
-
-def markdownToHtml():
-    """Converts Markdown file to HTML.
-
-    Args:
-        markdown_file (str): The path to the Markdown file.
-        output_file (str): The path to the output HTML file.
-
-    Returns:
-        int: Exit code. 0 if successful, 1 if Markdown file is missing.
+def main():
+    """
+    Main function to check arguments and file existence.
     """
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html\n", file = sys.stderr)
-        exit(1)
+        print("Usage: ./markdown2html.py <Markdown file> <Output file>", file=sys.stderr)
+        sys.exit(1)
 
     markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
 
     if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}\n", file = sys.stderr)
-        exit(1)
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
 
-    exit(0)
+    sys.exit(0)
 
 if __name__ == "__main__":
-    markdownToHtml()
+    main()
