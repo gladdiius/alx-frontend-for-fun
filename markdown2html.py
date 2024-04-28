bb277721b4a018846bdc94d5f3dd3dd63903c629
elias <eliaseshetu28@gmail.com>
2024-04-28 21:01:37 +0300

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,23 @@
 import sys
-""" markdown html"""
+"""MARKDOWN CONVERTER"""
 
 
 def convert_markdown_to_html(markdown_text, output_file):
+    """
+    Convert Markdown content to HTML and write it to the output file.
+
+    Args:
+        markdown_text (str): The Markdown content to convert.
+        output_file (str): The name of the output HTML file.
+
+    Returns:
+        None
+    """
     try:
-        with open(output_file, 'r') as f:
-            pass
+        with open(output_file, 'w') as f:
+            f.write(f'<html><body>{markdown_text}</body></html>')
     except FileNotFoundError:
-        sys.stderr.write(f"Missing {markdown_file}\n")
+        sys.stderr.write(f"Missing {markdown_text}\n")
         sys.exit(1)
 
 
